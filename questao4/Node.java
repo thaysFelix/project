500c83a648c100f553653076c1c574714b9c2f51
thaysFelix <60678251+thaysFelix@users.noreply.github.com>
2021-07-26 14:46:13 -0300

    Add files via upload

---
+++ questao4/Node.java
@@ -0,0 +1,37 @@
+package arvore;
+
+public class Node {
+
+	protected int value;
+
+	public Node left;
+	public Node right;
+        public Node parent;
+
+	public Node(int value) {
+		this.value = value;
+	}
+        
+        public Node() {}
+
+	public boolean isLeaf() {
+		return left == null && right == null;
+	}
+
+	public boolean hasLeft() {
+		return left != null;
+	}
+
+	public boolean hasRight() {
+		return right != null;
+	}
+
+	public void setChilds(Node left, Node right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public String toString() {
+		return Integer.toString(value);
+	}
+}
