6c139f56ee5cacfbcefec38072ea5b7d9acca0cf
thaysFelix <60678251+thaysFelix@users.noreply.github.com>
2021-08-27 18:47:23 -0300

    Update Main.java

--- questao01/Main.java
+++ questao01/Main.java
@@ -1,17 +1,18 @@
-package Trabalho;
+package trabalho;
 
 public class Main {
 
     public static void main(String[] args) {
 
-      ToHeap<int> heap = new ToHeap<int>();
-      Integer[] vet = {40,87,2,90,1,100,30,20};
+      Heap<Integer> heap = new Heap<Integer>();
+      Integer[] vet = {40,87,2,90,1,100,30,20, 150, 200, 30, 10};
       heap.toHeap(vet);
 
       System.out.println("Depois: ");
       for(int i = 0; i < vet.length; i++) {
-          System.out.println(vet[i]);
-      }
+         System.out.println(vet[i]);
+     }
 
   }
+
 }
