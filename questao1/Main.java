78a32f3aa063bbafd6f8682087270d16ceb7b6fa
thaysFelix <60678251+thaysFelix@users.noreply.github.com>
2021-08-30 14:31:25 -0300

    Update Main.java

--- questao1/Main.java
+++ questao1/Main.java
@@ -4,7 +4,7 @@ public class Main {
         
         Heap heap = new Heap();
         Integer[] vet = {40,87,2,90,1,100,30,20,150,200,32,10};
-        //
+        
         heap.toHeap(vet);
         Heap.MeuComparador comparador = new Heap.MeuComparador();
         heap.toHeap(vet, comparador);
