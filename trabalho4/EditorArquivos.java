9f8f35cad6165bba076a18dd80fde48bc80ae88c
thaysFelix <60678251+thaysFelix@users.noreply.github.com>
2021-09-08 13:24:52 -0300

    Add files via upload

--- trabalho4/EditorArquivos.java
+++ trabalho4/EditorArquivos.java
@@ -7,12 +7,20 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+import javax.swing.text.BadLocationException;
 
 public class EditorArquivos extends javax.swing.JFrame {
+
+    BufferedReader buffRead;
+    File f;
+    String fileName;
 
     public EditorArquivos() {
         initComponents();
@@ -22,93 +30,124 @@ public class EditorArquivos extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuItem1 = new javax.swing.JMenuItem();
         jScrollPane1 = new javax.swing.JScrollPane();
-        TextArea1 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        conteudoArquivo = new javax.swing.JTextArea();
+        caminho = new javax.swing.JLabel();
+        linhaEditada = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        menu = new javax.swing.JMenu();
+        abrirArquivo = new javax.swing.JMenuItem();
+        botaoSalvar = new javax.swing.JMenuItem();
+
+        jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        TextArea1.setColumns(20);
-        TextArea1.setRows(5);
-        jScrollPane1.setViewportView(TextArea1);
-
-        jButton1.setText("OpenFile");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+        conteudoArquivo.setColumns(20);
+        conteudoArquivo.setRows(5);
+        conteudoArquivo.addCaretListener(new javax.swing.event.CaretListener() {
+            public void caretUpdate(javax.swing.event.CaretEvent evt) {
+                conteudoArquivoCaretUpdate(evt);
             }
         });
+        jScrollPane1.setViewportView(conteudoArquivo);
 
-        jButton2.setText("Save");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        caminho.setText("Caminho:");
+
+        linhaEditada.setText("Linha editada: ");
+
+        menu.setText("Arquivos");
+
+        abrirArquivo.setText("Abrir");
+        abrirArquivo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                abrirArquivoActionPerformed(evt);
             }
         });
+        menu.add(abrirArquivo);
+
+        botaoSalvar.setText("Salvar");
+        botaoSalvar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoSalvarActionPerformed(evt);
+            }
+        });
+        menu.add(botaoSalvar);
+
+        jMenuBar1.add(menu);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(111, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 614, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 614, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(caminho)
+                    .addComponent(linhaEditada))
+                .addContainerGap(111, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(26, 26, 26)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(caminho)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(linhaEditada)
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        try {
-                    
-            FileWriter textWriter = new FileWriter("C:\\Users\\Thays&Thalys\\Documents\\NetBeansProjects\\Trabalho4\\src\\trabalho4\\qualquercoisa.txt");
-
-            BufferedWriter buffWrite = new BufferedWriter(textWriter);
-            TextArea1.write(buffWrite);
-            buffWrite.close();
-            TextArea1.setText("");
-            TextArea1.requestFocus();
-
-           
-        } catch(Exception e) {
-            JOptionPane.showMessageDialog (null, e);
-        }
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void abrirArquivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_abrirArquivoActionPerformed
         JFileChooser chooserFile = new JFileChooser();
         chooserFile.showOpenDialog(null);
-        File f = chooserFile.getSelectedFile();
-        String fileName = f.getAbsolutePath();
-        try{
-            FileReader textReader = new FileReader(fileName); 
-           
-            BufferedReader buffRead = new BufferedReader(textReader);
-            TextArea1.read(buffRead, null);
+        f = chooserFile.getSelectedFile();
+        fileName = f.getAbsolutePath();
+        caminho.setText("Caminho: "+ fileName);
+        try {
+            FileReader textReader = new FileReader(fileName);
+
+            buffRead = new BufferedReader(textReader);
+            conteudoArquivo.read(buffRead, null);
             buffRead.close();
-            TextArea1.requestFocus();
-        
-        }catch(Exception e) {
-            JOptionPane.showMessageDialog (null, e);
+            conteudoArquivo.requestFocus();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_abrirArquivoActionPerformed
+
+    private void botaoSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoSalvarActionPerformed
+        String texto = conteudoArquivo.getText();
+
+        try {
+            FileWriter textWriter = new FileWriter(fileName);
+            textWriter.write(texto);
+            textWriter.close();
+            JOptionPane.showMessageDialog(null, "Texto salvo com sucesso");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }//GEN-LAST:event_botaoSalvarActionPerformed
+
+    private void conteudoArquivoCaretUpdate(javax.swing.event.CaretEvent evt) {//GEN-FIRST:event_conteudoArquivoCaretUpdate
+        try {
+            int linhaSendoEditada = conteudoArquivo.getLineOfOffset(conteudoArquivo.getCaretPosition()) + 1;
+            linhaEditada.setText("Linha editada: "+ String.valueOf(linhaSendoEditada));
+            
+        } catch (BadLocationException ex) {
+            Logger.getLogger(EditorArquivos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }//GEN-LAST:event_conteudoArquivoCaretUpdate
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -146,9 +185,14 @@ public class EditorArquivos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextArea TextArea1;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JMenuItem abrirArquivo;
+    private javax.swing.JMenuItem botaoSalvar;
+    private javax.swing.JLabel caminho;
+    private javax.swing.JTextArea conteudoArquivo;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel linhaEditada;
+    private javax.swing.JMenu menu;
     // End of variables declaration//GEN-END:variables
 }
